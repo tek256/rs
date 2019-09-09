c92651a8914a8c4d13b303434ddc6324345a195b
Devon <devon@tek256.com>
2019-09-09 12:36:30 -0700

    Added multiple file parsing.

--- parse.py
+++ parse.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 
 def dict_contains(test_dict, test_value):
@@ -7,22 +8,21 @@ def dict_contains(test_dict, test_value):
             return True
     return False
 
-
-with open('data.json') as json_file:
-    data = json.load(json_file)
-    unique_events = {}
+def parse_file(file_location, dictionary):
+    with open(file_location) as json_file:
+        data = json.load(json_file)
 
     for row in data:
         data_type = row['eventId']
-        if not dict_contains(unique_events, data_type):
+        if not dict_contains(dictionary, data_type):
             item_dict = {}
 
             for drop in row['drops']:
                 item_dict[drop['id']] = drop['qty']
 
-            unique_events[data_type] = item_dict
+            dictionary[data_type] = item_dict
         else:
-            item_dict = unique_events[data_type]
+            item_dict = dictionary[data_type]
 
             for drop in row['drops']:
                 d_id = drop['id']
@@ -33,12 +33,14 @@ with open('data.json') as json_file:
                     item_dict[d_id] = value
                 else:
                     item_dict[d_id] = d_qty
-            unique_events[data_type] = item_dict
+            dictionary[data_type] = item_dict
 
+
+def pretty_print(dictionary):
     with open('rsitems.json') as f:
         rs_list = json.load(f)
 
-        for key, row in unique_events.items():
+        for key, row in dictionary.items():
             print("--", key, "--")
             for item_id, qty in row.items():
                 item_name = ''
@@ -46,4 +48,14 @@ with open('data.json') as json_file:
                     if int(rs_key) == item_id:
                         item_name = rs_row['name']
                         break
-                print(item_name, ":", qty)
+                print(item_name, ":", qty)   
+
+
+
+file_count = len(sys.argv) - 1
+drop_dict = {}
+
+for f in range(1, len(sys.argv)):
+    parse_file(sys.argv[f], drop_dict)
+
+pretty_print(drop_dict)
