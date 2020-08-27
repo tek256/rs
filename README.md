360bf20e670ca38b561b32894995ee464e580b92
Devon <devon@tek256.com>
2020-08-26 23:00:20 -0700

    Cleaned up code, added options, updated readme

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 ## Usage
 - Copy / Clone repository onto computer  
-- Make sure Python 3 is installed  
+- Make sure Python 3 is installed (download [here](https://www.python.org/downloads/))
 - Copy JSON data file(s) into folder
 - In powershell navigate to folder (`cd insert/path/here/`)
 - run `./parse.py data1.json ... datan.json`
-## NOTE
+
+## Tips
+You can configure the script to combine data or not (not will be faster) as well as if to output the results to file (output.txt), what to name the output file, and if to output to command line. 
+
+## Notes
 If powershell gives you an error saying `cmdlet` not found, then run the following command: `$env:PATHEXT += ";.py"`. This will allow powershell to run python scripts as executables.
 
 The script will combine all data passed as arguments into combined.json
--- parse.py
+++ parse.py
@@ -51,25 +51,59 @@ def pretty_print(dictionary):
                         break
                 print(item_name, ":", qty)   
 
-def merge_files(dst, filepath):
-    result = list()
-    with open(filepath, 'r') as infile:
-        result.extend(json.load(infile))
 
-    with open(dst, 'w') as output:
-        json.dump(result, output);
+def pretty_output(dict, output_path):
+    with open('rsitems.json') as f:
+        rs_list = json.load(f)
 
+    out_file = open(output_path, "w")
+    for key, row in dict.items():
+        out_file.write(f"\n-- {key} --\n")
+        for item_id, qty in row.items():
+            item_name = ''
+            for rs_key, rs_row in rs_list.items():
+                if int(rs_key) == item_id:
+                    item_name = rs_row['name']
+                    break
+            out_file.write(f"{item_name}: {qty}\n") 
+    
 
+# --- CONFIGURATION --- 
+# If to combine json files
+combine_data = True
 
+# If to output results to a text file for future reference
+output_path = "output.txt"
+output_to_file = True
+
+# If to output to terminal/command line
+output_to_cmd = True
+
+# --- END OF CONFIGURATION --- 
+
+# Calculated things (not configuration)
 file_count = len(sys.argv) - 1
 drop_dict = {}
 result = list()
 
+# For each file passed on the command line
 for f in range(1, len(sys.argv)):
-    result.extend(parse_file(sys.argv[f], drop_dict))
-    print(sys.argv[f])
-    if(file_count > 1):
-        with open("combined.json", 'w') as output:
+    # If multiple files and combining 
+    if(file_count > 1 and combine_data):
+        # Parse file and add to resulting json array
+        result.extend(parse_file(sys.argv[f], drop_dict))
+    else:
+        parse_file(sys.argv[f], drop_dict)
+
+# Write the combined data
+if(combine_data and file_count > 1):
+    with open("combined.json", 'w') as output:
             json.dump(result, output)
 
-pretty_print(drop_dict)
+# If we should output to the command line 
+if(output_to_cmd):
+    pretty_print(drop_dict)
+
+# If we should output to file
+if(output_to_file):
+    pretty_output(drop_dict, output_path)
